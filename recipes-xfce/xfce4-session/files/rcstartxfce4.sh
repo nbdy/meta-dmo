a372d9fb5ac74ec5bf2a4b33738cd744f531b69f
S. Fricke <sfricke@data-modul.com>
2013-07-03 15:58:39 +0200

    [#213] some bits changed for stop mode

--- recipes-xfce/xfce4-session/files/rcstartxfce4.sh
+++ recipes-xfce/xfce4-session/files/rcstartxfce4.sh
@@ -12,22 +12,23 @@ disable_dpms () {
 case "$1" in
 	start)
 		echo -n "Starting XFCE4"
-		killall X >/dev/null 2&>1
+		killall X >/dev/null 2>&1
 		sleep 1
-		start-stop-daemon -S -x /usr/bin/startxfce4 >/dev/null 2&>1  &
+		start-stop-daemon -S -x /usr/bin/startxfce4 >/dev/null 2>&1  &
                 disable_dpms
 		echo "."
 		;;
 	stop)
 		echo -n "Stopping XFCE4"
-		start-stop-daemon -K --oknodo -x /usr/bin/startxfce4 >/dev/null 2&>1  &
+		killall X >/dev/null 2>&1
+		# start-stop-daemon -K --oknodo -x /usr/bin/startxfce4 >/dev/null 2&>1  &
 		echo "."
 		;;
 	restart)
 		echo -n "Restarting XFCE4"
-		killall X >/dev/null 2&>1
+		killall X >/dev/null 2>&1
 		sleep 1
-		start-stop-daemon -S -x /usr/bin/startxfce4 >/dev/null 2&>1  &
+		start-stop-daemon -S -x /usr/bin/startxfce4 >/dev/null 2>&1  &
                 disable_dpms
 		echo "."
 		;;
