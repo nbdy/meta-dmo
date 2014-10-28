aa4d1d9e3a7f58f1c3e1b56f65faa03a39f201e8
Zahari Doychev <zahari.doychev@linux.com>
2014-10-28 11:16:15 +0100

    [meta-dmo] gst: change playback scripts for coda
    The video playback scripts for gstreamer are adjusted to match the new coda
    driver.
    
    Signed-off-by: Zahari Doychev <zahari.doychev@linux.com>

--- recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-1920x1080.sh
+++ recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-1920x1080.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-video="$1"
-
-if [ ! -e "${video}" ]; then
-    echo "Videofile \"${video}\" does not exists"
-    exit
+file=$1
+if [[ -z $file ]]; then
+	echo "Usage: $0 <video file>"
+	exit 1
 fi
 
-/usr/bin/gst-launch-0.11  -v \
-    filesrc location="${video}" \
-    ! qtdemux name=d d.video_0 \
-    ! h264parse \
-    ! video/x-h264,stream-format=byte-stream \
-    ! v4l2filter device=/dev/video4 min-queued=1 \
-    ! video/x-raw,format=YV12,width=1920,height=1088 \
-    ! v4l2sink device=/dev/video1 crop-top=0  crop-height=1080 overlay-height=1080
+/usr/bin/gst-launch-1.0  -v \
+        filesrc location=$file \
+        ! qtdemux name=d d.video_0 \
+        ! h264parse \
+        ! video/x-h264,stream-format=byte-stream \
+        ! v4l2filter device=/dev/video/by-name/coda-decoder-3 min-queued=1 \
+        ! video/x-raw,format=YV12,width=1920,height=1088 \
+        ! identity sync=true \
+        ! v4l2sink device=/dev/video/by-name/voutbg-0 crop-top=0 \
+        crop-height=1080 overlay-height=1080 sync=false
 
--- recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-800x480.sh
+++ recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-800x480.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-video="$1"
-
-if [ ! -e "${video}" ]; then
-    echo "Videofile \"${video}\" does not exists"
-    exit
+file=$1
+if [[ -z $file ]]; then
+	echo "Usage: $0 <video file>"
+	exit 1
 fi
 
-/usr/bin/gst-launch-0.11  -v \
-    filesrc location="${video}" \
-    ! qtdemux name=d d.video_0 \
-    ! h264parse \
-    ! video/x-h264,stream-format=byte-stream \
-    ! v4l2filter device=/dev/video4 min-queued=1 \
-    ! video/x-raw,format=I420,width=800,height=480 \
-    ! v4l2sink device=/dev/video1
+/usr/bin/gst-launch-1.0  -v \
+        filesrc location=$file \
+        ! qtdemux name=d d.video_0 \
+        ! h264parse \
+        ! video/x-h264,stream-format=byte-stream \
+        ! v4l2filter device=/dev/video/by-name/coda-decoder-3 min-queued=1 \
+        ! video/x-raw,format=YV12,width=800,height=480 \
+        ! identity sync=true \
+        ! v4l2sink device=/dev/video/by-name/voutbg-0 crop-top=0 \
+	crop-height=480 overlay-height=480 sync=false
+
