4be61481d34180746411748c3c9e21016f57ba89
Sebastian Wezel <swezel@data-modul.com>
2015-02-24 13:43:32 +0100

    videoscripts: The videoscripts now check if the coda-decoder3 and voutbg-1 device symlink is present. If not, the code-decoder-2 and voutbg-0 is used instead
    Signed-off-by: Sebastian Wezel <swezel@data-modul.com>

--- recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-1920x1080.sh
+++ recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-1920x1080.sh
@@ -6,14 +6,25 @@ if [[ -z $file ]]; then
 	exit 1
 fi
 
+coda=coda-decoder-3
+vout=voutbg-1
+
+if [[ ! -L /dev/video/by-name/$coda ]]; then
+	coda=coda-decoder-2
+fi
+
+if [[ ! -L /dev/video/by-name/$vout ]]; then
+	vout=voutbg-0
+fi
+
 /usr/bin/gst-launch-1.0  -v \
         filesrc location=$file \
         ! qtdemux name=d d.video_0 \
         ! h264parse \
         ! video/x-h264,stream-format=byte-stream \
-        ! v4l2filter device=/dev/video/by-name/coda-decoder-3 min-queued=1 \
+        ! v4l2filter device=/dev/video/by-name/$coda min-queued=1 \
         ! video/x-raw,format=YV12,width=1920,height=1088 \
         ! identity sync=true \
-        ! v4l2sink device=/dev/video/by-name/voutbg-0 crop-top=0 \
+        ! v4l2sink device=/dev/video/by-name/$vout crop-top=0 \
         crop-height=1080 overlay-height=1080 sync=false
 
--- recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-800x480.sh
+++ recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-800x480.sh
@@ -6,14 +6,25 @@ if [[ -z $file ]]; then
 	exit 1
 fi
 
+coda=coda-decoder-3
+vout=voutbg-1
+
+if [[ ! -L /dev/video/by-name/$coda ]]; then
+	coda=coda-decoder-2
+fi
+
+if [[ ! -L /dev/video/by-name/$vout ]]; then
+	vout=voutbg-0
+fi
+
 /usr/bin/gst-launch-1.0  -v \
         filesrc location=$file \
         ! qtdemux name=d d.video_0 \
         ! h264parse \
         ! video/x-h264,stream-format=byte-stream \
-        ! v4l2filter device=/dev/video/by-name/coda-decoder-3 min-queued=1 \
+        ! v4l2filter device=/dev/video/by-name/$coda min-queued=1 \
         ! video/x-raw,format=YV12,width=800,height=480 \
         ! identity sync=true \
-        ! v4l2sink device=/dev/video/by-name/voutbg-0 crop-top=0 \
-	crop-height=480 overlay-height=480 sync=false
+        ! v4l2sink device=/dev/video/by-name/$vout crop-top=0 \
+        crop-height=480 overlay-height=480 sync=false
 
