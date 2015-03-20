1f108d15dcc0423d8f1f0b17e85f6efef7ce198b
Sebastian Wezel <swezel@data-modul.com>
2015-03-20 12:29:04 +0100

    dmo-video: Fixed a bug in the lvds version of the scripts
    Signed-off-by: Sebastian Wezel <swezel@data-modul.com>

--- recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-1920x1080.sh
+++ recipes-multimedia/dmo-gst-videoscripts/files/dmo-video-1920x1080.sh
@@ -7,14 +7,9 @@ if [[ -z $file ]]; then
 fi
 
 coda=coda-decoder-3
-vout=voutbg-1
 
 if [[ ! -L /dev/video/by-name/$coda ]]; then
 	coda=coda-decoder-2
-fi
-
-if [[ ! -L /dev/video/by-name/$vout ]]; then
-	vout=voutbg-0
 fi
 
 /usr/bin/gst-launch-1.0  -v \
@@ -25,6 +20,6 @@ fi
         ! v4l2filter device=/dev/video/by-name/$coda min-queued=1 \
         ! video/x-raw,format=YV12,width=1920,height=1088 \
         ! identity sync=true \
-        ! v4l2sink device=/dev/video/by-name/$vout crop-top=0 \
+        ! v4l2sink device=/dev/video/by-name/voutbg-0 crop-top=0 \
         crop-height=1080 overlay-height=1080 sync=false
 
